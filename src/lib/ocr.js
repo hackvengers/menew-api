e9a8622b7eb95ce9fac4adb0bc28b3ac0cacac2e
jun097kim <jun097kim@gmail.com>
2019-05-14 17:17:49 +0900

    Fix to preserve Exif metadata when resizing

--- src/lib/ocr.js
+++ src/lib/ocr.js
@@ -2,18 +2,25 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const mkdirp = require('mkdirp');
 
 const DIR_PATH = path.resolve(__dirname, '../uploads');
+const RESIZE_DIR_PATH = path.join(DIR_PATH, 'resize');
 
 const getOcrText = async imageUrl => {
+  // 디렉토리가 존재하지 않으면 생성(mkdir -p)
+  if (!fs.existsSync(RESIZE_DIR_PATH)) {
+    mkdirp.sync(RESIZE_DIR_PATH);
+  }
+
   await sharp(path.resolve(DIR_PATH, imageUrl))
     .resize(800)
+    .withMetadata()
     .toFile(path.resolve(DIR_PATH, 'resize', imageUrl));
 
   const fileStream = fs.createReadStream(
     path.resolve(DIR_PATH, 'resize', imageUrl)
   );
-
   return new Promise((resolve, reject) => {
     request.post(
       {
@@ -27,6 +34,7 @@ const getOcrText = async imageUrl => {
       },
       (err, httpResponse, body) => {
         if (err) {
+          console.log(err);
           throw err;
           reject(err);
         }
